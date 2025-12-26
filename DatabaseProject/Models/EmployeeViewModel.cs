b1b7cc4de5a05fadc2b9eaca83a5ae51b0108234
furkanatak37 <138790686+furkanatak37@users.noreply.github.com>
2025-12-26 20:06:30 +0300

    fdsf

---
+++ DatabaseProject/Models/EmployeeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatabaseProject.Models;
+
+public class EmployeeViewModel
+{
+    public int PersonID { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+}
